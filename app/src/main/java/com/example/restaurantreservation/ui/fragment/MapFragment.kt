2c89a9b355c57b840ba894f390f12b626ff27948
Maciej Pomiankiewicz <maciej.pomiankiewicz@tedee.com>
2020-04-28 16:55:00 +0200

    detect user location & zoom to it on map

--- app/src/main/java/com/example/restaurantreservation/ui/fragment/MapFragment.kt
+++ app/src/main/java/com/example/restaurantreservation/ui/fragment/MapFragment.kt
@@ -1,19 +1,30 @@
 package com.example.restaurantreservation.ui.fragment
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProvider
 import com.example.restaurantreservation.R
 import com.example.restaurantreservation.ui.viewmodel.MapViewModel
 import com.example.restaurantreservation.viewmodel.ViewModelProviderFactory
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.tasks.Task
 import dagger.android.support.DaggerFragment
 import kotlinx.android.synthetic.main.fragment_map.*
 import javax.inject.Inject
+
 
 class MapFragment : DaggerFragment(), OnMapReadyCallback {
 
@@ -21,6 +32,9 @@ class MapFragment : DaggerFragment(), OnMapReadyCallback {
 
     @Inject
     lateinit var providerFactory: ViewModelProviderFactory
+
+    private var currentLocation: Location? = null
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,20 +46,74 @@ class MapFragment : DaggerFragment(), OnMapReadyCallback {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val mapFragment = childFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
         viewModel = ViewModelProvider(this, providerFactory).get(MapViewModel::class.java)
         test_button.setOnClickListener {
             viewModel.initialize()
         }
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireActivity())
+        fetchLocation()
     }
 
     override fun onMapReady(googleMap: GoogleMap?) {
-        googleMap?.let {
-            googleMap.uiSettings?.setAllGesturesEnabled(true)
-            googleMap.uiSettings?.isMyLocationButtonEnabled = true
-            googleMap.uiSettings?.isZoomControlsEnabled = true
+        val latLng = currentLocation?.let { LatLng(it.latitude, it.longitude) }
+        googleMap?.apply {
+            uiSettings?.setAllGesturesEnabled(true)
+            uiSettings?.isMyLocationButtonEnabled = true
+            uiSettings?.isZoomControlsEnabled = true
+            isMyLocationEnabled = true
+            animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, DEFAULT_ZOOM))
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE && grantResults.contains(PackageManager.PERMISSION_GRANTED)) {
+            fetchLocation()
+        } else {
+            Toast.makeText(
+                requireContext(),
+                "Location is needed for app to work!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun fetchLocation() {
+        if (!locationPermissionsGranted()) {
+            requestPermissions(
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_CODE
+            )
+            return
+        }
+        val task: Task<Location> = fusedLocationProviderClient.lastLocation
+        task.addOnSuccessListener { location ->
+            if (location != null) {
+                currentLocation = location
+                val mapFragment = childFragmentManager
+                    .findFragmentById(R.id.map) as SupportMapFragment
+                mapFragment.getMapAsync(this)
+            }
+        }
+    }
+
+    private fun locationPermissionsGranted() =
+        ActivityCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+
+    companion object {
+        const val REQUEST_CODE = 175
+        const val DEFAULT_ZOOM = 14f
     }
 }
